6ef54070d9e89ae1b8e667f35a66f3eef16a3cbe
Sudheer <a@b1.com>
2025-03-28 14:37:54 -0400

    Initial Commit Project Setup

---
+++ src/main/java/org/eureka/stockAnalytics/StockAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package org.eureka.stockAnalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAnalyticsApplication.class, args);
+	}
+
+}
